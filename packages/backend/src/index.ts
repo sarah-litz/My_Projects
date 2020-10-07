21afc6570520ffd89788c5cf642dff7c0d74463d
huro6781 <70212733+huro6781@users.noreply.github.com>
2020-10-07 10:41:27 -0600

    added basic apollo server integration

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,36 @@
 import express from 'express';
 
-const app = express();
+const { ApolloServer, gql } = require('apollo-server');
 
-app.use('*', (req, res) => res.send('Hello!'));
+const typeDefs = gql`
+  # Basic login option
+  type Login {
+    userName: String
+    passWord: String
+  }
 
-app.listen(4000, () => {
-  console.log('Starting backend on port 4000.');
+  type Query {
+    users: [Login]
+  }
+`;
+
+// for test purposes
+const users = [
+  {
+    userName: 'user1',
+    passWord: 'pass'
+  }
+];
+////
+
+const resolvers = {
+  Query: {
+    users: () => users
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen(4000, () => {
+  console.log(`Starting backend on port 4000.`);
 });
