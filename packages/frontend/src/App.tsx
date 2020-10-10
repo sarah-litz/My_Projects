f8e709eb612eec71faa4bb10a13dc85330475ea0
Kyle Pfromer <kpfromer2@gmail.com>
2020-10-09 18:08:18 -0600

    feat: add react bootstrap/react router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { HomePage } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* Like a regular switch statement for routes */}
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          {/* Default route when is not found above (404 page) */}
+          <Route>
+            <Layout>404 Error</Layout>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
