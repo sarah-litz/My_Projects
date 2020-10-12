04150aecff9f4f03c1787b090dfaf7d61c268dd5
Kyle Pfromer <kpfromer2@gmail.com>
2020-10-12 13:34:28 -0600

    feat: update sleep datum for type-graphql

--- packages/backend/src/models/SleepDatum.ts
+++ packages/backend/src/models/SleepDatum.ts
@@ -1,23 +1,37 @@
+import { Max, Min } from 'class-validator';
+import { Field, Float, ID, Int, ObjectType } from 'type-graphql';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import User from './User';
 
 // https://docs.google.com/document/d/17bUPFTNd6bemJvxNaTDMQnjmGFMEpZtOVwDJ_8RfR5E/edit
-@Entity()
+@ObjectType()
+@Entity('sleepdata')
 export default class SleepDatum {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   public id!: number;
 
-  @Column({ nullable: true })
+  @Min(0)
+  @Field(() => Float, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   public totalHours?: number;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   public didDream?: boolean;
 
-  @Column({ nullable: true })
+  @Min(0)
+  @Max(10)
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   public anxiety?: number;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   public feltRested?: boolean;
+
+  @Column({ type: 'date' })
+  public date!: Date;
 
   @ManyToOne(() => User, (user) => user.sleepData)
   public user!: User;
