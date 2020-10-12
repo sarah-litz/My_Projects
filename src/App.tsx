769ab8c975592bdb210918ce851e4790215d51cc
sarah-litz <66532212+sarah-litz@users.noreply.github.com>
2020-10-12 17:25:33 -0600

    pre - monday meeting (10-12-20)
    containing rough draft of frontend Login file

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './Components/Login'; 
+
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+          <p>
+          
+          </p>
+          
+        </header>
+        <Login></Login>
+    </div>
+  );
+}
+
+export default App;
