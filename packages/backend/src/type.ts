5ed0686257c974510dc89bdd911b44e8c33861e2
Kyle Pfromer <kpfromer2@gmail.com>
2020-11-04 17:57:51 -0700

    fix: user resolver not creating user and update to use helper

--- packages/backend/src/type.ts
+++ packages/backend/src/type.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 
 export type ContextType = {
+  /**
+   * The current user's details (if logged in)
+   */
+  me?: {
+    id: string;
+  };
   req: Request;
   res: Response;
 };
